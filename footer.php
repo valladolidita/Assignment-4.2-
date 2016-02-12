3fb5ac57e4362eb47bd5bdac6e5d576e28dee76c
valladolidita <kelly@Kellys-MacBook-Pro.local>
2016-02-12 17:14:15 -0500

    Week 4 Assignment 4.2
    MCAD PHP/ Wordpress Class

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package pressedsqueezed
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+			<h1>Contact Us</h1>
+			<p>Pressed and Squeezed</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
