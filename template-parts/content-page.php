2a28224dd38f426216e664f50c431a327b4babf8
valladolidita <kelly@Kellys-MacBook-Pro.local>
2016-02-13 16:22:16 -0500

    Assignment 4.2 --- FINAL
    Added css rules for: -webkit-box-sizing, -moz-box-sizing, and box-sizing

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,16 +10,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<!--	<header class="entry-header">
 		<?php 
 
 			the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	</header> .entry-header -->
 
 	<div class="entry-content">
 		<?php
 			
 			the_post_thumbnail();
+
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 
 			the_content();
 
